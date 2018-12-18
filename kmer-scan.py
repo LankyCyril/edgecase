005f05a208f09ec1c698e01316b5e3b375d6c705
LankyCyril <cyril@ro.ru>
2018-12-18 16:51:08 -0500

    Circular kmer identity

--- kmer-scan.py
+++ kmer-scan.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
+from itertools import product
+from regex import compile, IGNORECASE
 
 ARG_RULES = {
     ("input-fastq",): {
@@ -27,8 +29,34 @@ ARG_RULES = {
     }
 }
 
+class KmerIdentity:
+    """Hold all possible kmers of length k and their groupings by circular shift"""
+    _one2many = {}
+    _many2one = {}
+ 
+    def __init__(self, k=6, alphabet=list("ACGT")):
+        """Generate all possible kmers and group into identical by circular shift"""
+        for kmer in map("".join, product(alphabet, repeat=k)):
+            shifted_kmers = {kmer[i:]+kmer[:i] for i in range(k)}
+            for shifted_kmer in shifted_kmers:
+                if shifted_kmer in self._one2many:
+                    anchor_kmer = shifted_kmer
+                    self._one2many[anchor_kmer] |= shifted_kmers
+                    break
+            else:
+                anchor_kmer = shifted_kmer
+                self._one2many[anchor_kmer] = shifted_kmers
+            for shifted_kmer in shifted_kmers:
+                self._many2one[shifted_kmer] = anchor_kmer
+ 
+    def pattern(self, kmer, flags=IGNORECASE):
+        """Return a pattern matching kmer and its shifts"""
+        anchor_kmer = self._many2one[kmer]
+        shifted_kmers = self._one2many[anchor_kmer]
+        return compile(r'|'.join(shifted_kmers), flags=flags)
+
 def main(args):
-    args = get_args()
+    kmer_identity = KmerIdentity(k=len(args.kmer))
     return 0
 
 if __name__ == "__main__":
