d22dd34741c8626e7c1654cd7ea7ea24069ef859
LankyCyril <cyril@ro.ru>
2019-01-31 17:53:32 -0500

    Dev: more refactoring

--- edgecase/kmerscanner.py
+++ edgecase/kmerscanner.py
@@ -3,9 +3,10 @@ from numpy import zeros, array, cumsum
 from multiprocessing import Pool
 from edgecase.io import ReadFileChain
 from pysam import FastxFile
-from itertools import islice
 from functools import partial
 from tqdm import tqdm
+from argparse import ArgumentParser
+from sys import stderr
 
 
 def get_edge_density(read, pattern, head, tail):
@@ -68,14 +69,66 @@ def pattern_scanner(read_iterator, pattern, cutoff, window_size, head, tail, num
         )
 
 
-def fastq_scanner(fastqs, pattern, cutoff, window_size, head, tail, num_reads, jobs):
-    """Thin wrapper over pattern_scanner() providing read_iterator from fastq file"""
-    with ReadFileChain(fastqs, FastxFile) as read_iterator:
-        # only take the first num_reads entries (`None` takes all):
-        capped_read_iterator = islice(read_iterator, num_reads)
-        # parallelize scan:
-        yield from pattern_scanner(
-            capped_read_iterator, pattern, jobs=jobs,
-            window_size=window_size, head=head, tail=tail,
-            cutoff=cutoff, num_reads=num_reads
+USAGE = "python3 {} [options] fastq > txt".format(__file__)
+
+ARG_RULES = {
+    ("fastqs",): {
+        "help": "name of input FASTQ file",
+        "nargs": "+"
+    },
+    ("--kmer",): {
+        "help": "target kmer sequence (default TTAGGG)",
+        "default": "TTAGGG", "metavar": "M"
+    },
+    ("--head-test",): {
+        "help": "length of head to use for density filter (if specified)",
+        "default": None, "type": int, "metavar": "H"
+    },
+    ("--tail-test",): {
+        "help": "length of tail to use for density filter (if specified)",
+        "default": None, "type": int, "metavar": "T"
+    },
+    ("-c", "--cutoff"): {
+        "help": "use hard cutoff for density (default None)",
+        "default": None, "type": float, "metavar": "C"
+    },
+    ("-w", "--window-size"): {
+        "help": "size of the rolling window (default 120)",
+        "default": 120, "type": int, "metavar": "W"
+    },
+    ("-n", "--num-reads"): {
+        "help": "expected number of reads in input (for progress display)",
+        "default": None, "type": int, "metavar": "N"
+    },
+    ("-j", "--jobs"): {
+        "help": "number of jobs to run in parallel (default 1)",
+        "default": 1, "type": int, "metavar": "J"
+    }
+}
+
+
+if __name__ == "__main__":
+    # parse and check arguments:
+    parser = ArgumentParser(usage=USAGE)
+    for rule_args, rule_kwargs in ARG_RULES.items():
+        parser.add_argument(*rule_args, **rule_kwargs)
+    args = parser.parse_args()
+    if (args.head_test is not None) and (args.tail_test is not None):
+        raise ValueError("Can only specify one of --head, --tail")
+    elif (args.cutoff is not None) and (args.head_test is None) and (args.tail_test is None):
+        raise ValueError("--cutoff has no effect without a head/tail test")
+    elif ((args.head_test is not None) or (args.tail_test is not None)) and (args.cutoff is None):
+        print("Warning: --head/--tail has no effect without --cutoff", file=stderr)
+    # dispatch data to subroutines:
+    pattern = "FIXME"
+    # scan fastq for target kmer query, parallelizing on reads:
+    with ReadFileChain(args.fastqs, FastxFile) as read_iterator:
+        scanner = pattern_scanner(
+            read_iterator, pattern,
+            window_size=args.window_size, head=args.head, tail=args.tail,
+            cutoff=args.cutoff, num_reads=args.num_reads, jobs=args.jobs
         )
+        # output densities of reads that pass filter:
+        for read_name, density_array, passes_filter in scanner:
+            if passes_filter:
+                print(read_name, *density_array, sep="\t")
