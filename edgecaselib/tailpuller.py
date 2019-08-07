d9fed0678ba6b3cf30bcd57e4bf0f882fd19652e
LankyCyril <cyril@ro.ru>
2019-08-07 19:32:00 -0400

    Dev: simplify tailpuller logic

--- edgecaselib/tailpuller.py
+++ edgecaselib/tailpuller.py
@@ -9,7 +9,7 @@ from copy import deepcopy
 
 
 def load_index(index_filename):
-    """Load FAI and ECX indices"""
+    """Load ECX index"""
     if not isfile(index_filename):
         raise FileNotFoundError(index_filename)
     else:
@@ -20,31 +20,29 @@ def load_index(index_filename):
 
 def make_ecx_filter_dict(ecx):
     """Make simple dictionary for filtering purposes from the ECX"""
-    slim_ecx = ecx[["rname", "pos", "flag", "prime", "rc"]]
+    slim_ecx = ecx[["rname", "pos", "flag", "prime"]]
     rnames = set(slim_ecx["rname"])
     return {
-        rname: slim_ecx[
-                slim_ecx["rname"]==rname
-            ].drop(columns="rname").set_index(["prime", "rc"])
+        rname: slim_ecx[slim_ecx["rname"]==rname].drop(columns="rname")
         for rname in rnames
     }
 
 
-def get_anchor_pos(reference_pos, cigartuples, cigarpos):
+def get_terminal_pos(entry, cigarpos):
     """Calculate the position of clipped start/end of read relative to the reference"""
-    if not cigartuples: # no CIGAR available
+    if not entry.cigartuples: # no CIGAR available
         return None
     # measure the None (clipped) stretch on the left (0) or right (-1):
-    cigartype, clip = cigartuples[cigarpos]
+    cigartype, clip = entry.cigartuples[cigarpos]
     if (cigartype != 4) and (cigartype != 5): # not a soft/hard clip
         clip = 0
     # determine location of start/end of the read relative to reference:
     if cigarpos == 0: # start
-        return reference_pos - clip
+        return entry.reference_start - clip
     elif cigarpos == -1: # end
-        return reference_pos + clip
+        return entry.reference_end + clip
     else:
-        raise ValueError("get_anchor_pos(): cigarpos can only be 0 or -1")
+        raise ValueError("get_terminal_pos(): cigarpos can only be 0 or -1")
 
 
 def updated_entry(entry, flags, is_q=False):
@@ -60,20 +58,18 @@ def filter_entries(bam_data, ecxfd, flag_filter):
     """Only pass reads extending past regions specified in the ECX"""
     for entry in bam_data:
         if (entry.flag & flag_filter == 0) and (entry.reference_name in ecxfd):
-            ecx_entry = ecxfd[entry.reference_name]
-            p_anchor_pos = get_anchor_pos( # pos of start relative to reference
-                entry.reference_start, entry.cigartuples, cigarpos=0
-            )
+            ecx_t = ecxfd[entry.reference_name]
+            # find positions of start and end of read relative to reference:
+            p_pos = get_terminal_pos(entry, cigarpos=0)
+            q_pos = get_terminal_pos(entry, cigarpos=-1)
+            # collect available ECX flags:
             p_flags = set( # ECX flags where anchor is to right of read start
-                ecx_entry[ecx_entry["pos"]>=p_anchor_pos].loc[[5, "-"], "flag"]
+                ecx_t.loc[(ecx_t["pos"]>=p_pos) & (ecx_t["prime"]==5), "flag"]
             )
             if p_flags:
                 yield updated_entry(entry, p_flags)
-            q_anchor_pos = get_anchor_pos( # pos of end relative to reference
-                entry.reference_end, entry.cigartuples, cigarpos=-1
-            )
             q_flags = set( # ECX flags where anchor is to left of read end
-                ecx_entry[ecx_entry["pos"]<q_anchor_pos].loc[[3, "-"], "flag"]
+                ecx_t.loc[(ecx_t["pos"]<q_pos) & (ecx_t["prime"]==3), "flag"]
             )
             if q_flags:
                 yield updated_entry(entry, q_flags, is_q=True)
