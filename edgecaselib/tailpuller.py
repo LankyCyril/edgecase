0cc7fdd7b781a32e792915fe4c92ae5238e54c13
LankyCyril <cyril@ro.ru>
2019-08-07 18:46:44 -0400

    Dev: tailpuller: convert ECX to dict of dataframes

--- edgecaselib/tailpuller.py
+++ edgecaselib/tailpuller.py
@@ -2,70 +2,81 @@ from sys import stdout
 from os.path import isfile
 from edgecaselib.util import ReadFileChain
 from pysam import AlignmentFile
-from pandas import read_csv, read_fwf
+from pandas import read_fwf
 from functools import reduce
 from operator import __or__
 
 
-def load_index(basename):
+def load_index(index_filename):
     """Load FAI and ECX indices"""
-    fai_filename, ecx_filename = basename + ".fai", basename + ".ecx"
-    if not isfile(fai_filename):
-        raise FileNotFoundError(fai_filename)
-    elif not isfile(ecx_filename):
-        raise FileNotFoundError(ecx_filename)
+    if not isfile(index_filename):
+        raise FileNotFoundError(index_filename)
     else:
-        ecx = read_fwf(ecx_filename, skiprows=1)
+        ecx = read_fwf(index_filename, skiprows=1)
         ecx.columns = [c.lstrip("#") for c in ecx.columns]
-        fai = read_csv(
-            fai_filename, sep="\t",
-            usecols=(0,1), index_col=0, names=["rname", "length"]
-        )
-        return ecx, dict(fai["length"])
+        return ecx
 
 
-def filter_entries(bam_data, ecx, flag_filter):
+def make_ecx_filter_dict(ecx):
+    """Make simple dictionary for filtering purposes from the ECX"""
+    slim_ecx = ecx[["rname", "pos", "flag", "prime", "rc"]]
+    rnames = set(slim_ecx["rname"])
+    return {
+        rname: slim_ecx[
+                slim_ecx["rname"]==rname
+            ].drop(columns="rname").set_index(["prime", "rc"])
+        for rname in rnames
+    }
+
+
+def get_anchor_pos(reference_pos, cigartuples, cigarpos):
+    # measure the None (clipped) stretch on the left (0) or right (-1):
+    if not cigartuples: # no CIGAR available
+        return None
+    cigartype, clip = cigartuples[cigarpos]
+    if (cigartype != 4) and (cigartype != 5): # not a soft/hard clip
+        clip = 0
+    # determine location of start/end of the read relative to reference:
+    if cigarpos == 0: # start
+        return reference_pos - clip
+    elif cigarpos == -1: # end
+        return reference_pos + clip
+    else:
+        raise ValueError("get_anchor_pos(): cigarpos can only be 0 or -1")
+
+
+def filter_entries(bam_data, ecxfd, flag_filter):
     """Only pass reads extending past regions specified in the ECX"""
-    ecx_rnames = set(ecx["rname"])
     for entry in bam_data:
-        passes_flags = (entry.flag & flag_filter == 0)
-        if passes_flags and (entry.reference_name in ecx_rnames):
-            # measure the None (clipped) stretch on the left:
-            p_cigartype, p_clip = entry.cigartuples[0]
-            if (p_cigartype != 4) and (p_cigartype != 5):
-                p_clip = 0
-            # determine location of the start of the read relative to reference:
-            p_anchor_pos = entry.reference_start - p_clip
-            # find flags in ECX where anchor is to the right of the read start:
-            p_flags = set(ecx.loc[
-                (ecx["rname"]==entry.reference_name) & (ecx["prime"]==5) &
-                (ecx["pos"]>=p_anchor_pos), "flag"
-            ])
-            # measure the None (clipped) stretch on the right:
-            q_cigartype, q_clip = entry.cigartuples[-1]
-            if (q_cigartype != 4) and (q_cigartype != 5):
-                q_clip = 0
-            # determine location of the end of the read relative to reference:
-            q_anchor_pos = entry.reference_end + q_clip
-            # find flags in ECX where anchor is to the left of the read end:
-            q_flags = set(ecx.loc[
-                (ecx["rname"]==entry.reference_name) & (ecx["prime"]==3) &
-                (ecx["pos"]<=q_anchor_pos), "flag"
-            ])
+        if (entry.flag & flag_filter == 0) and (entry.reference_name in ecxfd):
+            ecx_entry = ecxfd[entry.reference_name]
+            p_anchor_pos = get_anchor_pos( # pos of start relative to reference
+                entry.reference_start, entry.cigartuples, cigarpos=0
+            )
+            p_flags = set( # ECX flags where anchor is to right of read start
+                ecx_entry[ecx_entry["pos"]>=p_anchor_pos].loc[[5, "-"], "flag"]
+            )
+            q_anchor_pos = get_anchor_pos( # pos of end relative to reference
+                entry.reference_end, entry.cigartuples, cigarpos=-1
+            )
+            q_flags = set( # ECX flags where anchor is to left of read end
+                ecx_entry[ecx_entry["pos"]<=q_anchor_pos].loc[[3, "-"], "flag"]
+            )
             extra_flags = p_flags | q_flags
-            if extra_flags: # update entry flags with edgeCase flags and yield
+            if extra_flags: # update entry flags with ECX flags and yield
                 entry.flag |= reduce(__or__, extra_flags)
                 if q_flags: # add the is_q flag
                     entry.flag |= 0x8000
                 yield entry
 
 
-def main(bams, reference, flag_filter, file=stdout, **kwargs):
+def main(bams, index, flag_filter, file=stdout, **kwargs):
     # use header of first input file (NB! fragile):
     with AlignmentFile(bams[0]) as bam:
         print(str(bam.header).rstrip("\n"), file=file)
     # dispatch data to subroutines:
-    ecx, reflens = load_index(reference)
+    ecx = load_index(index)
     with ReadFileChain(bams, AlignmentFile) as bam_data:
-        for entry in filter_entries(bam_data, ecx, flag_filter):
+        ecxfd = make_ecx_filter_dict(ecx)
+        for entry in filter_entries(bam_data, ecxfd, flag_filter):
             print(entry.to_string(), file=file)
