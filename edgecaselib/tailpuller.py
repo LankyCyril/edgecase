641f266aebad6cce29135b0f47b705cfac21b9b0
LankyCyril <cyril@ro.ru>
2019-08-07 22:41:50 -0400

    ECX as a nested dict for faster filtering

--- edgecaselib/tailpuller.py
+++ edgecaselib/tailpuller.py
@@ -8,29 +8,35 @@ from operator import __or__
 from copy import deepcopy
 
 
-def load_index(index_filename):
-    """Load ECX index"""
+def load_index(index_filename, as_filter_dict=False):
+    """Load ECX index; convert to simpler dictionary for filtering if requested"""
     if not isfile(index_filename):
         raise FileNotFoundError(index_filename)
     else:
-        return read_csv(index_filename, sep="\t", skiprows=1, escapechar="#")
-
-
-def make_ecx_filter_dict(ecx):
-    """Make simple dictionary for filtering purposes from the ECX"""
-    slim_ecx = ecx[["rname", "pos", "flag", "prime"]]
-    rnames = set(slim_ecx["rname"])
-    return {
-        rname: slim_ecx[slim_ecx["rname"]==rname].drop(columns="rname")
-        for rname in rnames
-    }
+        ecx = read_csv(index_filename, sep="\t", skiprows=1, escapechar="#")
+        if as_filter_dict:
+            slim_ecx = ecx[["rname", "pos", "flag", "prime"]]
+            rnames = set(slim_ecx["rname"])
+            return {
+                rname: {
+                    5: slim_ecx[
+                        (slim_ecx["rname"]==rname) & (slim_ecx["prime"]==5)
+                    ].drop(columns=["rname", "prime"]),
+                    3: slim_ecx[
+                        (slim_ecx["rname"]==rname) & (slim_ecx["prime"]==3)
+                    ].drop(columns=["rname", "prime"])
+                }
+                for rname in rnames
+            }
+        else:
+            return ecx
 
 
 def get_terminal_pos(entry, cigarpos):
     """Calculate the position of clipped start/end of read relative to the reference"""
     if not entry.cigartuples: # no CIGAR available
         return None
-    # measure the None (clipped) stretch on the left (0) or right (-1):
+    # measure the clipped stretch on the left (0) or right (-1):
     cigartype, clip = entry.cigartuples[cigarpos]
     if (cigartype != 4) and (cigartype != 5): # not a soft/hard clip
         clip = 0
@@ -56,19 +62,17 @@ def filter_entries(bam_data, ecxfd, flag_filter):
     """Only pass reads extending past regions specified in the ECX"""
     for entry in bam_data:
         if (entry.flag & flag_filter == 0) and (entry.reference_name in ecxfd):
-            ecx_t = ecxfd[entry.reference_name]
             # find positions of start and end of read relative to reference:
             p_pos = get_terminal_pos(entry, cigarpos=0)
             q_pos = get_terminal_pos(entry, cigarpos=-1)
-            # collect available ECX flags:
-            p_flags = set( # ECX flags where anchor is to right of read start
-                ecx_t.loc[(ecx_t["pos"]>=p_pos) & (ecx_t["prime"]==5), "flag"]
-            )
+            # collect ECX flags where anchor is to right of read start:
+            ecx_t_p5 = ecxfd[entry.reference_name][5]
+            p_flags = set(ecx_t_p5.loc[ecx_t_p5["pos"]>=p_pos, "flag"])
             if p_flags:
                 yield updated_entry(entry, p_flags)
-            q_flags = set( # ECX flags where anchor is to left of read end
-                ecx_t.loc[(ecx_t["pos"]<q_pos) & (ecx_t["prime"]==3), "flag"]
-            )
+            # collect ECX flags where anchor is to left of read end
+            ecx_t_p3 = ecxfd[entry.reference_name][3]
+            q_flags = set(ecx_t_p3.loc[ecx_t_p3["pos"]<q_pos, "flag"])
             if q_flags:
                 yield updated_entry(entry, q_flags, is_q=True)
 
@@ -78,8 +82,7 @@ def main(bams, index, flag_filter, file=stdout, **kwargs):
     with AlignmentFile(bams[0]) as bam:
         print(str(bam.header).rstrip("\n"), file=file)
     # dispatch data to subroutines:
-    ecx = load_index(index)
+    ecxfd = load_index(index, as_filter_dict=True)
     with ReadFileChain(bams, AlignmentFile) as bam_data:
-        ecxfd = make_ecx_filter_dict(ecx)
         for entry in filter_entries(bam_data, ecxfd, flag_filter):
             print(entry.to_string(), file=file)
