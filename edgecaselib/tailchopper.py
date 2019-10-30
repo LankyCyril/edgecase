12449bd958fc3ebb511a1493ecb68500e301fe4e
LankyCyril <cyril@ro.ru>
2019-10-29 20:26:15 -0400

    tailchopper: descriptive errors

--- edgecaselib/tailchopper.py
+++ edgecaselib/tailchopper.py
@@ -36,12 +36,14 @@ def update_aligned_segment(entry, map_pos, start=None, end=None):
 def cigar_chopper(entry, ecx, integer_target):
     """Return only clipped part of sequence"""
     is_q = (entry.flag & 0x8000 != 0)
+    error = None
     if is_q:
         cigar_clip = search(r'(\d+[SH])+$', entry.cigarstring)
     else:
         cigar_clip = search(r'^(\d+[SH])+', entry.cigarstring)
     if not cigar_clip:
         update_aligned_segment(entry, None, 0, 0)
+        error = "No clipped sequence"
     else:
         clip_length = sum(
             int(clip) for clip in split(r'[SH]', cigar_clip.group())
@@ -56,7 +58,8 @@ def cigar_chopper(entry, ecx, integer_target):
                 update_aligned_segment(entry, map_pos, None, clip_length)
         else:
             update_aligned_segment(entry, None, 0, 0)
-    return entry
+            error = "No clipped sequence"
+    return entry, error
 
 
 def find_map_and_cut_positions(entry, anchor_pos, is_q):
@@ -102,6 +105,7 @@ def find_map_and_cut_positions(entry, anchor_pos, is_q):
 def relative_chopper(entry, ecx, integer_target):
     """Return only part of sequence past the anchor"""
     is_q = (entry.flag & 0x8000 != 0)
+    error = None
     prime = 3 if is_q else 5
     indexer = (
         (ecx["rname"]==entry.reference_name) &
@@ -112,20 +116,22 @@ def relative_chopper(entry, ecx, integer_target):
         raise ValueError("Ambiguous index entry: {}".format(anchor_positions))
     elif len(anchor_positions) == 0:
         update_aligned_segment(entry, 0, 0)
+        error = "No anchor data in index"
     else:
         anchor_pos = anchor_positions.iloc[0]
         try:
             map_pos, cut_pos = find_map_and_cut_positions(
                 entry, anchor_pos, is_q
             )
-        except ValueError:
+        except ValueError as e:
             update_aligned_segment(entry, None, 0, 0)
+            error = str(e)
         else:
             if is_q:
                 update_aligned_segment(entry, map_pos, cut_pos, None)
             else:
                 update_aligned_segment(entry, map_pos, None, -cut_pos)
-    return entry
+    return entry, error
 
 
 def main(bam, index, flags, flags_any, flag_filter, min_quality, target, jobs=1, file=stdout, **kwargs):
@@ -140,10 +146,13 @@ def main(bam, index, flags, flags_any, flag_filter, min_quality, target, jobs=1,
         samfilters = [flags, flags_any, flag_filter, min_quality]
         for entry in filter_bam(alignment, samfilters):
             if entry.query_sequence:
-                chopped_entry = chopper(
+                chopped_entry, error = chopper(
                     entry, ecx, integer_target
                 )
                 if chopped_entry.query_sequence:
                     print(chopped_entry.to_string(), file=file)
                 else:
-                    print("Unchoppable:", entry.query_name, file=stderr)
+                    error_message = "Skipping {} ({})".format(
+                        entry.query_name, error or "Zero length chop"
+                    )
+                    print(error_message, file=stderr)
