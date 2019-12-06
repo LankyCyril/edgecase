c1e5f720f210603c4b27a0c479e81e8f72a5d513
LankyCyril <cyril@ro.ru>
2019-12-06 15:39:23 -0500

    Iterative MEME

--- edgecaselib/meme.py
+++ edgecaselib/meme.py
@@ -2,9 +2,16 @@ from sys import stdout, stderr
 from pysam import AlignmentFile, FastxFile
 from os import path
 from re import search
-from subprocess import Popen, PIPE, check_output
+from subprocess import PIPE, check_output, run
 from edgecaselib.formats import filter_bam
 from edgecaselib.util import get_executable
+from pandas import DataFrame, concat
+from uuid import uuid4
+from regex import finditer, IGNORECASE
+from itertools import count
+
+MEME_FMT = r'^MOTIF\s([A-Za-z]+)\sMEME-([0-9]+).*llr.*E-value\s*=\s*([0-9e.-]+)'
+MEME_REGEX_FMT = r'Motif\s[A-Za-z]+\sMEME-([0-9]+)\sregular\sexpression'
 
 
 from contextlib import contextmanager
@@ -24,7 +31,7 @@ def guess_bg_fmt(background):
         return "sam"
 
 
-def interpret_args(fmt, fasta_get_markov, bioawk, samtools, meme, background):
+def interpret_args(fmt, fasta_get_markov, meme, background):
     """Parse and check arguments"""
     if fmt == "sam":
         manager = AlignmentFile
@@ -37,37 +44,43 @@ def interpret_args(fmt, fasta_get_markov, bioawk, samtools, meme, background):
         error_mask = "No {} found, needed to generate background"
         if get_executable("fasta-get-markov", fasta_get_markov, False) is None:
             raise ValueError(error_mask.format("fasta-get-markov"))
-        if get_executable("bioawk", bioawk, False) is None:
-            raise ValueError(error_mask.format("bioawk"))
-        if get_executable("samtools", samtools, False) is None:
-            raise ValueError(error_mask.format("samtools"))
     return (
         manager,
         get_executable("fasta-get-markov", fasta_get_markov, False),
-        get_executable("bioawk", bioawk, False),
-        get_executable("samtools", samtools, False),
         get_executable("meme", meme),
         bg_fmt
     )
 
 
-def convert_background(sam, tempdir, fasta_get_markov, bioawk, samtools, max_order=6):
+def interpret_lengths(lengths):
+    """Convert a lengths string into an array of minw and maxw"""
+    lengths_array = []
+    for span in lengths.split(","):
+        fields = span.split("-")
+        if len(fields) > 2:
+            raise ValueError("Incorrect syntax: '{}'".format(lengths))
+        else:
+            lengths_array.append([int(fields[0]), int(fields[-1])])
+    return lengths_array
+
+
+def convert_background(sam, tempdir, fasta_get_markov, max_order=6, max_entries=1000000):
     """Convert a SAM/BAM file into Markov background for MEME"""
-    print("SAM/BAM -> HMM", file=stderr, flush=True)
-    samtools_view = Popen(
-        [samtools, "view", "-F3844", sam], stdout=PIPE
-    )
-    bioawk_conv = Popen(
-        [bioawk, "-c", "sam", '{print ">"$qname; print $seq}'],
-        stdin=samtools_view.stdout, stdout=PIPE
-    )
-    hmm = check_output(
+    print("SAM/BAM -> HMM...", file=stderr, flush=True)
+    fasta_lines = []
+    with AlignmentFile(sam) as alignment:
+        for i, entry in enumerate(alignment):
+            if i >= max_entries:
+                break
+            else:
+                fasta_lines.extend([">"+entry.qname, entry.seq])
+    hmm = run(
         [fasta_get_markov, "-m", str(max_order)],
-        stdin=bioawk_conv.stdout
+        input="\n".join(fasta_lines), encoding="ascii", stdout=PIPE
     )
     bfile = path.join(tempdir, "bfile")
     with open(bfile, mode="wt") as bfile_handle:
-        print(hmm.decode(), file=bfile_handle)
+        print(hmm.stdout, file=bfile_handle)
     print("...done", file=stderr, flush=True)
     return bfile
 
@@ -84,27 +97,104 @@ def convert_input(bam, manager, tempdir, samfilters):
     return fasta
 
 
-def run_meme(meme, jobs, readfile, background, minw, maxw, evt, tempdir):
-    """Run the MEME binary with preset parameters"""
-    check_output([
-        meme, "-p", str(jobs), "-dna", "-mod", "anr",
-        "-minw", str(minw), "-maxw", str(maxw),
-        "-minsites", "2", "-evt", str(evt),
-        "-bfile", background, "-oc", tempdir, readfile
-    ])
+def alpha_inversion(motif):
+    """Get alphanumerically smallest inversion of motif"""
+    return min(motif[i:]+motif[:i] for i in range(len(motif)))
 
 
-def main(readfile, fmt, flags, flags_any, flag_filter, min_quality, fasta_get_markov, bioawk, samtools, background, meme, minw, maxw, evt, jobs=1, file=stdout.buffer, **kwargs):
-    # parse arguments
-    manager, fasta_get_markov, bioawk, samtools, meme, bg_fmt = interpret_args(
-        fmt, fasta_get_markov, bioawk, samtools, meme, background
+def parse_meme_output(meme_txt, expect_nmotifs):
+    """Parse brief MEME output"""
+    with open(meme_txt) as meme_report_handle:
+        meme_report = meme_report_handle.readlines()
+    meme_df_as_list = []
+    for line in meme_report:
+        header_matcher = search(MEME_FMT, line)
+        if header_matcher:
+            meme_df_as_list.append([
+                alpha_inversion(header_matcher.group(1)),
+                int(header_matcher.group(2)), float(header_matcher.group(3))
+            ])
+    if len(meme_df_as_list) == 0:
+        return None, True
+    stop_flag = (len(meme_df_as_list) < expect_nmotifs)
+    meme_df = DataFrame(data=meme_df_as_list, columns=["motif", "meme_id", "e"])
+    meme_df["regex"] = None
+    for i, line in enumerate(meme_report):
+        regex_header_matcher = search(MEME_REGEX_FMT, line)
+        if regex_header_matcher:
+            _id = int(regex_header_matcher.group(1))
+            _regex = meme_report[i+2].strip()
+            meme_df.loc[meme_df["meme_id"]==_id, "regex"] = _regex
+    meme_df = meme_df.drop(columns="meme_id")
+    return meme_df.groupby(["motif", "regex"], as_index=False).min(), stop_flag
+
+
+def find_and_mask_motifs(current_readfile, tempdir, meme_output_dir, expect_nmotifs):
+    """Parse the MEME output"""
+    meme_report, stop_flag = parse_meme_output(
+        path.join(meme_output_dir, "meme.txt"), expect_nmotifs
     )
+    if meme_report is None:
+        return None, current_readfile, True
+    maskable_regex = r'|'.join(set(meme_report["regex"])).lower()
+    next_readfile = path.join(tempdir, str(uuid4()) + ".fa")
+    with FastxFile(current_readfile) as unmasked:
+        with open(next_readfile, mode="wt") as masked:
+            for entry in unmasked:
+                seq_as_list = list(entry.sequence)
+                match_iter = finditer(
+                    maskable_regex, entry.sequence,
+                    overlapped=True, flags=IGNORECASE
+                )
+                for match in match_iter:
+                    s, e = match.span()
+                    seq_as_list[s:e] = ["N"] * (e - s)
+                print(">" + entry.name, file=masked)
+                print("".join(seq_as_list), file=masked)
+    return meme_report, next_readfile, stop_flag
+
+
+def run_meme(meme, jobs, readfile, background, lengths_array, nmotifs, evt, tempdir):
+    """Run the MEME binary with preset parameters"""
+    current_readfile = readfile
+    meme_reports = []
+    for minw, maxw in lengths_array:
+        for it in count():
+            meme_output_dir = path.join(
+                tempdir, str(minw)+"-"+str(maxw)+"."+str(it)
+            )
+            check_output([
+                meme, "-p", str(jobs), "-dna", "-mod", "anr", "-minsites", "1",
+                "-minw", str(minw), "-maxw", str(maxw), "-bfile", background,
+                "-nmotifs", str(nmotifs), "-evt", str(evt), "-brief", "0",
+                "-oc", meme_output_dir, current_readfile
+            ])
+            next_meme_report, current_readfile, stop_flag = find_and_mask_motifs(
+                current_readfile, tempdir, meme_output_dir, nmotifs
+            )
+            if next_meme_report is not None:
+                meme_reports.append(next_meme_report)
+            if stop_flag:
+                break
+    return concat(meme_reports, axis=0)
+
+
+def main(readfile, fmt, flags, flags_any, flag_filter, min_quality, fasta_get_markov, background, meme, lengths, nmotifs, evt, jobs=1, file=stdout, **kwargs):
+    # parse arguments
+    manager, fasta_get_markov, meme, bg_fmt = interpret_args(
+        fmt, fasta_get_markov, meme, background
+    )
+    lengths_array = interpret_lengths(lengths)
     with TemporaryDirectory() as tempdir:
         if bg_fmt == "sam": # will need to convert SAM to HMM
             background = convert_background(
-                background, tempdir, fasta_get_markov, bioawk, samtools
+                background, tempdir, fasta_get_markov
             )
         if manager != FastxFile: # will need to convert SAM to fastx
             samfilters = [flags, flags_any, flag_filter, min_quality]
             readfile = convert_input(readfile, manager, tempdir, samfilters)
-        run_meme(meme, jobs, readfile, background, minw, maxw, evt, tempdir)
+        combined_meme_report = run_meme(
+            meme, jobs, readfile, background, lengths_array,
+            nmotifs, evt, tempdir
+        )
+    combined_meme_report.to_csv(file, sep="\t", index=False)
