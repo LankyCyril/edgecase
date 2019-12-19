822a89fe3170645cee6c055c5cc58c3156c7290a
LankyCyril <cyril@ro.ru>
2019-12-19 10:19:19 -0500

    Save LDs, remove pvals from clustermaps

--- edgecaselib/levenshtein.py
+++ edgecaselib/levenshtein.py
@@ -89,6 +89,11 @@ def calculate_chromosome_lds(chrom, entries):
     return lds.fillna(1)
 
 
+def save_lds(lds, output_dir, chrom):
+    if output_dir:
+        lds.to_csv(path.join(output_dir, chrom+"-matrix.tsv"), sep="\t")
+
+
 def generate_clustermap(lds, metric="euclidean", method="ward", cmap=CLUSTERMAP_CMAP, vmax=CLUSTERMAP_VMAX):
     try:
         cm = clustermap(
@@ -147,42 +152,39 @@ def get_clusters(lds, linkage):
         return c1_names, c2_names, c1_pval, c2_pval, silh_score
 
 
-def format_pval(pval):
-    if isnan(pval):
-        return "N/A"
-    elif pval >= .00001:
-        return "{:.5f}".format(pval)
-    else:
-        return "{:.1e}".format(pval)
-
-
-def display_pvals(c1_pval, c2_pval):
-    return "$p_1$={}, $p_2$={}".format(
-        format_pval(c1_pval), format_pval(c2_pval)
-    )
+def warn_about_unsupported_hierarchy(chrom):
+    msg = "({}): not implemented: complex clustering hierarchy or too few reads"
+    print("Warning", msg.format(chrom), file=stderr)
 
 
 def generate_kmerscanner_file(kmerscanner_file, c1_names, c2_names, output_dir, chrom):
-    kmerscanner_dat = read_csv(kmerscanner_file, sep="\t")
-    kmerscanner_dat_hap1 = kmerscanner_dat[
-        kmerscanner_dat["#name"].isin(c1_names)
-    ].copy()
-    kmerscanner_dat_hap1["chrom"] = kmerscanner_dat_hap1["chrom"].apply(
-        lambda s: s + ":haplotype 1"
+    clustering_successful = (
+        (output_dir is not None) and (kmerscanner_file is not None) and
+        (len(c1_names) != 0) and (len(c2_names) != 0)
     )
-    kmerscanner_dat_hap2 = kmerscanner_dat[
-        kmerscanner_dat["#name"].isin(c2_names)
-    ].copy()
-    kmerscanner_dat_hap2["chrom"] = kmerscanner_dat_hap2["chrom"].apply(
-        lambda s: s + ":haplotype 2"
-    )
-    haplo_dat = concat(
-        [kmerscanner_dat_hap1, kmerscanner_dat_hap2], axis=0
-    )
-    haplo_dat.to_csv(
-        path.join(output_dir, chrom+".dat.gz"), compression="gzip",
-        sep="\t", index=False
-    )
+    if clustering_successful:
+        kmerscanner_dat = read_csv(kmerscanner_file, sep="\t")
+        kmerscanner_dat_hap1 = kmerscanner_dat[
+            kmerscanner_dat["#name"].isin(c1_names)
+        ].copy()
+        kmerscanner_dat_hap1["chrom"] = kmerscanner_dat_hap1["chrom"].apply(
+            lambda s: s + ":haplotype 1"
+        )
+        kmerscanner_dat_hap2 = kmerscanner_dat[
+            kmerscanner_dat["#name"].isin(c2_names)
+        ].copy()
+        kmerscanner_dat_hap2["chrom"] = kmerscanner_dat_hap2["chrom"].apply(
+            lambda s: s + ":haplotype 2"
+        )
+        haplo_dat = concat(
+            [kmerscanner_dat_hap1, kmerscanner_dat_hap2], axis=0
+        )
+        haplo_dat.to_csv(
+            path.join(output_dir, chrom+".dat.gz"), compression="gzip",
+            sep="\t", index=False
+        )
+    else:
+        warn_about_unsupported_hierarchy(chrom)
 
 
 def generate_report(report_rows, adj="bonferroni"):
@@ -200,53 +202,39 @@ def generate_report(report_rows, adj="bonferroni"):
     return report
 
 
-def generate_pdf(cm, c1_pval, c2_pval, silh_score, output_dir, chrom, cmap=CLUSTERMAP_CMAP, vmax=CLUSTERMAP_VMAX):
-    cm.ax_col_dendrogram.clear()
-    cm.ax_col_dendrogram.imshow(
-        vstack([linspace(0, 1, 256)]*2), aspect="auto", cmap=cmap
-    )
-    gp = cm.ax_col_dendrogram.get_position()
-    cm.ax_col_dendrogram.set(
-        position=[
-            gp.x0*.25+gp.x1*.75, gp.y0*.55+gp.y1*.45, gp.x1*.1-gp.x0*.1, .015
-        ],
-        zorder=float("inf")
-    )
-    cm.ax_col_dendrogram.text(
-        x=-672, y=.8, va="center", ha="left", fontsize=19, s="Distance:  0"
-    )
-    cm.ax_col_dendrogram.text(
-        x=272, y=.8, va="center", ha="left", fontsize=19, s="{}+".format(vmax)
-    )
-    cm.ax_col_dendrogram.set_axis_off()
-    silh_text = "N/A" if isnan(silh_score) else "{:.3f}".format(silh_score)
-    cm.ax_col_dendrogram.text(
-        x=-672, y=3, va="top", ha="left", fontsize=19,
-        s="Silhouette score: "+silh_text
-    )
-    cm.ax_col_dendrogram.text(
-        x=-672, y=6.6, va="top", ha="left", fontsize=19,
-        s=display_pvals(c1_pval, c2_pval)
-    )
-    cm.ax_heatmap.text(
-        x=0, y=0, va="bottom", ha="left", fontsize=20,
-        s=shorten_chrom_name(chrom)+"\n"
-    )
-    filename = path.join(output_dir, chrom+".pdf")
-    cm.fig.savefig(filename, bbox_inches="tight")
-
-
-def batch_generate_pdfs(cms, report, output_dir):
+def generate_pdf(cm, silh_score, output_dir, chrom, cmap=CLUSTERMAP_CMAP, vmax=CLUSTERMAP_VMAX):
     if output_dir:
-        for _, report_entry in report.iterrows():
-            chrom = report_entry["#chrom"]
-            if chrom in cms:
-                generate_pdf(
-                    cms[chrom], report_entry["cluster1_p_adjusted"],
-                    report_entry["cluster2_p_adjusted"],
-                    report_entry["silhouette_score"],
-                    output_dir, chrom
-                )
+        cm.ax_col_dendrogram.clear()
+        cm.ax_col_dendrogram.imshow(
+            vstack([linspace(0, 1, 256)]*2), aspect="auto", cmap=cmap
+        )
+        gp = cm.ax_col_dendrogram.get_position()
+        cm.ax_col_dendrogram.set(
+            position=[
+                gp.x0*.25+gp.x1*.75, gp.y0*.5+gp.y1*.46, gp.x1*.1-gp.x0*.1, .015
+            ],
+            zorder=float("inf")
+        )
+        cm.ax_col_dendrogram.text(
+            x=-672, y=.8, va="center", ha="left", fontsize=19,
+            s="Distance:  0"
+        )
+        cm.ax_col_dendrogram.text(
+            x=272, y=.8, va="center", ha="left", fontsize=19,
+            s="{}+".format(vmax)
+        )
+        cm.ax_col_dendrogram.set_axis_off()
+        silh_text = "N/A" if isnan(silh_score) else "{:.3f}".format(silh_score)
+        cm.ax_col_dendrogram.text(
+            x=-672, y=3, va="top", ha="left", fontsize=19,
+            s="Silhouette score: "+silh_text
+        )
+        cm.ax_col_dendrogram.text(
+            x=-672, y=-4.9, va="top", ha="left", fontsize=19,
+            s=shorten_chrom_name(chrom)
+        )
+        filename = path.join(output_dir, chrom+".pdf")
+        cm.fig.savefig(filename, bbox_inches="tight")
 
 
 def hide_stats_warnings(state=True):
@@ -260,11 +248,6 @@ def hide_stats_warnings(state=True):
         resetwarnings()
 
 
-def warn_about_unsupported_hierarchy(chrom):
-    msg = "({}): not implemented: complex clustering hierarchy or too few reads"
-    print("Warning", msg.format(chrom), file=stderr)
-
-
 def main(bam, kmerscanner_file, output_dir, flags, flags_any, flag_filter, min_quality, jobs=1, file=stdout, **kwargs):
     switch_backend("pdf")
     hide_stats_warnings(True)
@@ -273,9 +256,10 @@ def main(bam, kmerscanner_file, output_dir, flags, flags_any, flag_filter, min_q
     samfilters = [flags, flags_any, flag_filter, min_quality]
     with AlignmentFile(bam) as alignment:
         bam_dict = load_bam_as_dict(alignment, samfilters)
-    cms, report_rows = {}, []
+    report_rows = []
     for chrom, entries in bam_dict.items():
         lds = calculate_chromosome_lds(chrom, entries)
+        save_lds(lds, output_dir, chrom)
         cm = generate_clustermap(lds)
         if cm is None:
             c1_names, c2_names = [], []
@@ -285,19 +269,14 @@ def main(bam, kmerscanner_file, output_dir, flags, flags_any, flag_filter, min_q
             c1_names, c2_names, c1_pval, c2_pval, silh_score = get_clusters(
                 lds, cm.dendrogram_row.linkage
             )
-            cms[chrom] = cm
-            if not isnan(silh_score):
-                if (output_dir is not None) and (kmerscanner_file is not None):
-                    generate_kmerscanner_file(
-                        kmerscanner_file, set(c1_names), set(c2_names),
-                        output_dir, chrom
-                    )
-            else:
-                warn_about_unsupported_hierarchy(chrom)
+            generate_pdf(cm, silh_score, output_dir, chrom)
+            generate_kmerscanner_file(
+                kmerscanner_file, set(c1_names), set(c2_names),
+                output_dir, chrom
+            )
         report_rows.append([
             chrom, len(c1_names), len(c2_names), silh_score, c1_pval, c2_pval
         ])
     report = generate_report(report_rows)
-    batch_generate_pdfs(cms, report, output_dir)
     print(report.to_csv(sep="\t", index=False, na_rep="NA"))
     hide_stats_warnings(False)
