d9388124d423c52c1037a2559685152ecead99f4
LankyCyril <cyril@ro.ru>
2020-01-02 12:11:07 -0500

    tools/masker.py: prototyping

--- tools/masker.py
+++ tools/masker.py
@@ -3,11 +3,25 @@ from sys import argv
 from pysam import AlignmentFile
 from regex import finditer, IGNORECASE
 from itertools import count
+from os import path
 
 
 def get_circular_motif(motif):
     return r'|'.join({motif[i:]+motif[:i] for i in range(len(motif))})
 
+
+def get_motifs_pattern(motif_description):
+    if path.isfile(motif_description):
+        with open(motif_description, mode="rt") as motif_handle:
+            return r'|'.join({
+                get_circular_motif(line.split()[1]*2)
+                for line in motif_handle if line[0] != "#"
+            })
+    else:
+        return r'|'.join({
+            get_circular_motif(kmer*2)
+            for kmer in motif_description.split("|")
+        })
 
 def iterate_bam(filename, f, F):
     with AlignmentFile(filename) as bam:
@@ -16,14 +30,27 @@ def iterate_bam(filename, f, F):
                 yield entry
 
 
-def main(filename, motif_filename, f=49152, F=3840, minlen=22):
+def cut_chunks(seq, positions_to_mask, minlen):
+    prev_pos, start, end = -1, 0, 0
+    for pos in range(len(seq)):
+        if pos not in positions_to_mask:
+            if pos == prev_pos + 1:
+                end = pos
+            else:
+                start = pos
+            prev_pos = pos
+        else:
+            if start < end:
+                if end - start + 1 >= minlen:
+                    yield seq[start:end+1]
+            start = end + 1
+
+
+def main(filename, motif_description, action="mask", f=49152, F=3840, minlen=22):
     minlen = int(minlen)
-    with open(motif_filename, mode="rt") as motif_handle:
-        motifs_pattern = r'|'.join({
-            get_circular_motif(line.split()[0]*2)
-            for line in motif_handle if line[0] != "#"
-        })
+    motifs_pattern = get_motifs_pattern(motif_description)
     cid = count(1)
+    total_masked, total_bases = 0, 0
     for entry in iterate_bam(filename, int(f), int(F)):
         positions_to_mask = set()
         matcher = finditer(
@@ -31,20 +58,22 @@ def main(filename, motif_filename, f=49152, F=3840, minlen=22):
         )
         for match in matcher:
             positions_to_mask |= set(range(match.start(), match.end()))
-        prev_pos, start, end = -1, 0, 0
-        for pos in range(len(entry.seq)):
-            if pos not in positions_to_mask:
-                if pos == prev_pos + 1:
-                    end = pos
-                else:
-                    start = pos
-                prev_pos = pos
-            else:
-                if start < end:
-                    if end - start + 1 >= minlen:
-                        print(">{}".format(next(cid)))
-                        print(entry.seq[start:end+1])
-                start = end + 1
+        if action == "cut":
+            for chunk in cut_chunks(entry.seq, positions_to_mask, minlen):
+                print(">{}\n{}".format(next(cid), chunk))
+        elif action == "mask":
+            raise NotImplementedError("`action` 'mask'")
+        elif action == "count":
+            print("{}\t{}".format(
+                len(positions_to_mask), len(entry.seq) - len(positions_to_mask)
+            ))
+        elif action == "total-fraction":
+            total_masked += len(positions_to_mask)
+            total_bases += len(entry.seq)
+        else:
+            raise ValueError("Unknown action: '{}'".format(action))
+    if action == "total-fraction":
+        print(total_masked, total_bases, total_masked/total_bases, sep="\t")
     return 0
 
 
