98801f6976ff4de8add13354ca4876cf4255d892
LankyCyril <cyril@ro.ru>
2021-02-09 19:14:31 -0500

    Options --min-map-overlap and --min-candidate-overlap

--- edgecaselib/basic_pipeline_longread.py
+++ edgecaselib/basic_pipeline_longread.py
@@ -16,53 +16,63 @@ Usage: {0} basic-pipeline-longread -x filename -o dirname [--force] [-j integer]
        {1}       [--title string] <bam>
 
 Output (in --output-dir):
-    * tailpuller.sam                 candidate reads
-    * tailchopper.sam                telomeric regions
-    * repeatfinder-p_arm.tsv         overrepresented motifs on the p arm
-    * repeatfinder-q_arm.tsv         overrepresented motifs on the q arm
-    * kmerscanner-p_arm.dat.gz       motif densities on the p arm
-    * kmerscanner-q_arm.dat.gz       motif densities on the q arm
-    * densityplot-p_arm.pdf          plot of motif densities on the p arm
-    * densityplot-q_arm.pdf          plot of motif densities on the q arm
+    * tailpuller.sam                       candidate reads
+    * tailchopper.sam                      telomeric regions
+    * repeatfinder-p_arm.tsv               overrepresented motifs on the p arm
+    * repeatfinder-q_arm.tsv               overrepresented motifs on the q arm
+    * kmerscanner-p_arm.dat.gz             motif densities on the p arm
+    * kmerscanner-q_arm.dat.gz             motif densities on the q arm
+    * densityplot-p_arm.pdf                plot of motif densities on the p arm
+    * densityplot-q_arm.pdf                plot of motif densities on the q arm
 
 Positional arguments:
-    <bam>                            name of input BAM/SAM file; must have a .bai index
+    <bam>                                  name of input BAM/SAM file; must have a .bai index
 
 Required options:
-    -x, --index [filename]           location of the reference .ecx index
-    -o, --output-dir [dirname]       name of the output directory (must exist)
+    -x, --index [filename]                 location of the reference .ecx index
+    -o, --output-dir [dirname]             name of the output directory (must exist)
 
 Options:
-    --force                          force overwrite files in --output-dir
-    -j, --jobs [integer]             number of parallel jobs (for jellyfish and kmerscanner) [default: 1]
-    -M, --max-read-length [integer]  maximum read length to consider when selecting lookup regions
-    -m, --min-overlap [integer]      minimum overlap of subtelomere to consider read [default: 0]
-    --target [targetspec]            an ECX flag for heads/tails [default: tract_anchor]
-    -n, --max-motifs [integer]       maximum number of motifs to report [default: 4]
-    --min-k [integer]                smallest target repeat length [default: 4]
-    --max-k [integer]                largest target repeat length [default: 16]
-    --max-p-adjusted [float]         cutoff adjusted p-value [default: .05]
-    --jellyfish [filename]           jellyfish binary (unless in $PATH)
-    --jellyfish-hash-size [string]   jellyfish initial hash size [default: 2G]
-    --bin-size [integer]             size of the window (for kmerscanner and densityplot) [default: 10]
-    --n-boot [integer]               number of bootstrap iterations for plotting [default: 1000]
-    --palette [palettespec]          custom palette for plotting motifs
-    --title [string]                 title for the density plots
+    --force                                force overwrite files in --output-dir
+    -j, --jobs [integer]                   number of parallel jobs (for jellyfish and kmerscanner) [default: 1]
+    -M, --max-read-length [integer]        maximum read length to consider when selecting lookup regions *
+    --min-map-overlap [integer]            minimum overlap of reference to consider read as mapped [default: 1] **
+    -m, --min-candidate-overlap [integer]  minimum overlap of subtelomere to consider read as candidate [default: 1] ***
+    --target [targetspec]                  an ECX flag for heads/tails [default: tract_anchor]
+    -n, --max-motifs [integer]             maximum number of motifs to report [default: 4]
+    --min-k [integer]                      smallest target repeat length [default: 4]
+    --max-k [integer]                      largest target repeat length [default: 16]
+    --max-p-adjusted [float]               cutoff adjusted p-value [default: .05]
+    --jellyfish [filename]                 jellyfish binary (unless in $PATH)
+    --jellyfish-hash-size [string]         jellyfish initial hash size [default: 2G]
+    --bin-size [integer]                   size of the window (for kmerscanner and densityplot) [default: 10]
+    --n-boot [integer]                     number of bootstrap iterations for plotting [default: 1000]
+    --palette [palettespec]                custom palette for plotting motifs
+    --title [string]                       title for the density plots
 
 Input filtering options:
-    -q, --min-quality [integer]      process only entries with this MAPQ or higher [default: 0]
+    -q, --min-quality [integer]      process only entries with this MAPQ or higher [default: 0] ****
 
 Notes:
-  * Depending on the aligner used, MAPQ of secondary reads may have been set to
-    zero regardless of real mapping quality; use this filtering option with
-    caution.
+   * Suggested value of --max-read-length for PacBio: 200000;
+     if not specified, will assume +infinity (will be slow).
+  ** Suggested value of --min-map-overlap for PacBio: 500;
+     if not specified, will assume 1.
+ *** Suggested value of --min-candidate-overlap for PacBio: 3000;
+     if not specified, will assume 1.
+**** Depending on the aligner used, MAPQ of secondary reads may have been set to
+     zero regardless of real mapping quality; use this filtering option with
+     caution.
 """
 
 __docopt_converters__ = [
     lambda jobs: int(jobs),
     lambda max_read_length:
         inf if (max_read_length is None) else int(max_read_length),
-    lambda min_overlap: 0 if (min_overlap is None) else int(min_overlap),
+    lambda min_map_overlap:
+        1 if (min_map_overlap is None) else int(min_map_overlap),
+    lambda min_candidate_overlap:
+        1 if (min_candidate_overlap is None) else int(min_candidate_overlap),
     lambda max_motifs: int(max_motifs),
     lambda min_k: int(min_k),
     lambda max_k: int(max_k),
@@ -90,8 +100,10 @@ __docopt_tests__ = {
 }
 
 
-get_tailpuller_kws = lambda min_quality, max_read_length, min_overlap, target, file: dict(
-    max_read_length=max_read_length, min_overlap=min_overlap, target={target},
+get_tailpuller_kws = lambda min_quality, max_read_length, min_map_overlap, min_candidate_overlap, target, file: dict(
+    max_read_length=max_read_length, target={target},
+    min_map_overlap=min_map_overlap,
+    min_candidate_overlap=min_candidate_overlap,
     flags=[0], flag_filter=[0], min_quality=min_quality,
     file=file,
 )
@@ -129,13 +141,14 @@ get_densityplot_kws = lambda f, F, q, bin_size, n_boot, palette, title, arm, fil
 )
 
 
-def main(bam, index, output_dir, jobs, max_read_length, min_overlap, max_motifs, target, min_k, max_k, max_p_adjusted, jellyfish, jellyfish_hash_size, bin_size, n_boot, palette, title, min_quality, **kwargs):
+def main(bam, index, output_dir, jobs, max_read_length, min_map_overlap, min_candidate_overlap, max_motifs, target, min_k, max_k, max_p_adjusted, jellyfish, jellyfish_hash_size, bin_size, n_boot, palette, title, min_quality, **kwargs):
     """basic pipeline: select reads, find enriched motifs, plot"""
     get_filename = lambda fn: path.join(output_dir, fn)
     tailpuller_sam = get_filename("tailpuller.sam")
     with open(tailpuller_sam, mode="wt") as sam:
         tailpuller.main(bam, index, **get_tailpuller_kws(
-            min_quality, max_read_length, min_overlap, target, sam,
+            min_quality, max_read_length, min_map_overlap,
+            min_candidate_overlap, target, sam,
         ))
     tailchopper_sam = get_filename("tailchopper.sam")
     with open(tailchopper_sam, mode="wt") as sam:
